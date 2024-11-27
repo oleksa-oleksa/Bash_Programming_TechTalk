5b2e26ba060a4ce7fe9b88da90fdf000b950e388
Oleksandra Baga <oleksandra.baga@gmail.com>
2024-11-27 18:20:01 +0100

    rebase user email

--- 01_std_combinations.sh
+++ 01_std_combinations.sh
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 #!/bin/bash
+=======
+>>>>>>> 22b34a8 (Initial commit, added stdin and stdout examples)
 # Sort the content of a file in reverse order 
 # and save the result to another file
 sort -r < input.txt > sorted_output.txt
